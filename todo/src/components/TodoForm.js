a6940bc53c39a1816e4a5af07b689cd7244d3768
marie-hotchkiss <mtknowlton67@gmail.com>
2020-05-21 11:51:43 -0700

    Added TodoForm

--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -1,5 +1,43 @@
 import React, { useState } from "react";
+import { Button } from " reactstrap";
 
+const TodoForm = props => {
+    const [newTodo, setNewTodo] = useState("");
+
+    const handleChanges = e => {
+        setNewTodo(e.target.value);
+    };
+
+    const addTodo = e => {
+        console.log(newTodo, "this is a newTodo in todo form");
+        console.log(props.todos, "this is props.todos");
+        e.preventDefault();
+        props.dispatch({type: "ADD_TODO", payload: newTodo });
+        setNewTodo("");
+
+    };
+    const removeTodo = e => {
+        e.preventDefault();
+        props.dispatch({ type: "REMOVE_TODO", payload: newTodo });
+    };
+    return (
+        <form>
+         <input 
+           className="forminput"
+           type="text"
+           name="item"
+           value={newTodo}
+           onChange={handleChanges}
+    />
+    <Button color="info" className="formButton" onClick={addTodo}>
+        Add New Task  
+    </Button> 
+    <Button color="success" className="formbutton" onClick={removeTodo}>
+        Clear Completed Tasks!
+      </Button>
+        </form>
+    );
+};
 
 
 
