3f6a5242fb8ea4bbb912a967efc89a01146fb9cf
marie-hotchkiss <mtknowlton67@gmail.com>
2020-05-21 11:40:56 -0700

    Modified todo/todoFOrm/todoList

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,11 +1,20 @@
-import React, {useState, useReducer } from 'react';
-
-import './App.css';
+import React, { useReducer } from 'react';
+import { reducer, initialState } from './reducers/reducer';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
+import "./App.css"
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log(state, "app state");
+
   return (
     <div className="App">
-     
+      <div className="App-header">
+        <h1> My Todo List App</h1>
+        <TodoForm dispatch={dispatch} todos={state}/>
+      </div>
+      <TodoList todos={state} dispatch={dispatch} />
     </div>
   );
 }
--- todo/src/reducers/todoReducers.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import App from './App';
-instanceof('renders without crashing', () => {
-    const div = documnet.creatElement('div');
-    ReactDom.render(<App />, div);
-    ReactDom.unmountComponentAtNoce(div);
-});
